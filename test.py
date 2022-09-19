cd6b94178e2ef6b6036d77ea088ab04f84865a94
willhk10 <wkeenan14@charlottesvilleschools.org>
2022-09-19 11:12:20 -0400

    yes

--- test.py
+++ test.py
@@ -1,25 +1,35 @@
-# Write your code here :-)
 import cv2
 import sys
 
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]
+cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
-print("Found {0} faces!").format(len(faces))
+video_capture = cv2.VideoCapture(0)
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
