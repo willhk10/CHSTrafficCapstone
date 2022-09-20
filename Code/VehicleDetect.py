af4302043a827a4ac76b8b655a7da5b98ea8ba88
willhk10 <wkeenan14@charlottesvilleschools.org>
2022-09-20 11:24:31 -0400

    vehicle

--- Code/VehicleDetect.py
+++ Code/VehicleDetect.py
@@ -20,3 +20,25 @@ ret, thresh = cv2.threshold(diff_image, 30, 255, cv2.THRESH_BINARY)
 # plot image after thresholding
 plt.imshow(thresh, cmap = 'gray')
 plt.show()
+
+# plot vehicle detection zone
+plt.imshow(dilated)
+cv2.line(dilated, (0, 80),(256,80),(100, 0, 0))
+plt.show()
+
+valid_cntrs = []
+
+for i,cntr in enumerate(contours):
+    x,y,w,h = cv2.boundingRect(cntr)
+    if (x <= 200) & (y >= 80) & (cv2.contourArea(cntr) >= 25):
+        valid_cntrs.append(cntr)
+
+# count of discovered contours        
+len(valid_cntrs)
+
+dmy = col_images[13].copy()
+
+cv2.drawContours(dmy, valid_cntrs, -1, (127,200,0), 2)
+cv2.line(dmy, (0, 80),(256,80),(100, 255, 255))
+plt.imshow(dmy)
+plt.show()
