7d86507f5cacbc9987d97a19baf7633ef9d70430
willhk10 <wkeenan14@charlottesvilleschools.org>
2022-09-20 12:01:11 -0400

    yes

--- Code/VehicleDetect.py
+++ Code/VehicleDetect.py
@@ -1,18 +1,20 @@
 # OpenCV Python program to detect cars in video frame
 # import libraries of python OpenCV 
 import cv2
+import numpy
   
 # capture frames from a video
-cap = cv2.VideoCapture('video.avi')
+cap = cv2.VideoCapture('C:/Users/wkeenan14/Documents/CHSTrafficCapstone/Media/video.avi')
   
 # Trained XML classifiers describes some features of some object we want to detect
-car_cascade = cv2.CascadeClassifier('cars.xml')
+car_cascade = cv2.CascadeClassifier('C:/Users/wkeenan14/Documents/CHSTrafficCapstone/cars.xml')
   
 # loop runs if capturing has been initialized.
 while True:
     # reads frames from a video
     ret, frames = cap.read()
-      
+
+    #print(len(frames))  
     # convert to gray scale of each frames
     gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
       
@@ -25,7 +27,7 @@ while True:
         cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
   
    # Display frames in a window 
-    cv2.imshow('video2', frames)
+    cv2.imshow('video', frames)
       
 # Wait for Esc key to stop
     if cv2.waitKey(33) == 27:
