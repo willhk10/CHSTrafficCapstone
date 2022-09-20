3bf2a8bd0e40f39bb6b7eadbe091e8f5c1ebc096
willhk10 <wkeenan14@charlottesvilleschools.org>
2022-09-20 11:23:35 -0400

    frames

--- Code/VehicleDetect.py
+++ Code/VehicleDetect.py
@@ -4,3 +4,19 @@ import cv2 # opencv library
 import numpy as np
 from os.path import isfile, join
 import matplotlib.pyplot as plt
+
+grayA = cv2.cvtColor(col_images[i], cv2.COLOR_BGR2GRAY)
+grayB = cv2.cvtColor(col_images[i+1], cv2.COLOR_BGR2GRAY)
+
+# plot the image after frame differencing
+plt.imshow(cv2.absdiff(grayB, grayA), cmap = 'gray')
+plt.show()
+
+diff_image = cv2.absdiff(grayB, grayA)
+
+# perform image thresholding
+ret, thresh = cv2.threshold(diff_image, 30, 255, cv2.THRESH_BINARY)
+
+# plot image after thresholding
+plt.imshow(thresh, cmap = 'gray')
+plt.show()
