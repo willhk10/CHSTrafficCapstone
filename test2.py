7105758dbecf5a45ffa3453026e1ffd4eb782779
willhk10 <wkeenan14@charlottesvilleschools.org>
2022-09-20 11:11:50 -0400

    yes

--- test2.py
+++ test2.py
@@ -1,21 +1,53 @@
-import cv2 as cv
+import cv2
+import sys
+import logging as log
+import datetime as dt
+from time import sleep
 
-# Read image from your local file system
-original_image = cv.imread('C:/Users/wkeenan14/Documents/CHSTrafficCapstone/abba.png')
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+log.basicConfig(filename='webcam.log',level=log.INFO)
 
-# Convert color image to grayscale for Viola-Jones
-grayscale_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+video_capture = cv2.VideoCapture(0)
+anterior = 0
 
-face_cascade = cv.CascadeClassifier("C:/Users/wkeenan14/Documents/CHSTrafficCapstone/haarcascade_frontalface_default")
-detected_faces = face_cascade.detectMultiScale(grayscale_image)
-for (column, row, width, height) in detected_faces:
-    cv.rectangle(
-        original_image,
-        (column, row),
-        (column + width, row + height),
-        (0, 255, 0),
-        2
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
     )
-cv.imshow('download.jpg', original_image)
-cv.waitKey(0)
-cv.destroyAllWindows()
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
