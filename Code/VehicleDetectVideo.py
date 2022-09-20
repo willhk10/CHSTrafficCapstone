bca758776a0e3e71bfa7ad40475871e1623e53ee
willhk10 <wkeenan14@charlottesvilleschools.org>
2022-09-20 14:11:51 -0400

    pleasehelp

--- Code/VehicleDetectVideo.py
+++ Code/VehicleDetectVideo.py
@@ -18,17 +18,16 @@ while True:
     # convert to gray scale of each frames
     gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray,(5,5),0)
-    dilated = cv2.dilate(frames, cv2.dilate(blur,np.ones((3,3))))
-  
+
     # Detects cars of different sizes in the input image
-    cars = car_cascade.detectMultiScale(gray, dilated, 1.1, 1)
+    cars = car_cascade.detectMultiScale(blur, 1.1, 1)
       
     # To draw a rectangle in each cars
     for (x,y,w,h) in cars:
         cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
-  
+    
    # Display frames in a window 
-    cv2.imshow('video', frames)
+    cv2.imshow('video', frames,)
       
 # Wait for Esc key to stop
     if cv2.waitKey(33) == 27:
