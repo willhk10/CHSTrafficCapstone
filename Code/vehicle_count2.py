8e59935272455be8515926a6bfa6d3af510f2b54
willhk10 <wkeenan14@charlottesvilleschools.org>
2022-10-04 11:07:38 -0400

    y

--- Code/vehicle_count2.py
+++ Code/vehicle_count2.py
@@ -16,14 +16,14 @@ up_line_position = middle_line_position - 15
 down_line_position = middle_line_position + 15
 
 # Store Coco Names in a list
-classesFile = "coco.names"
+classesFile = "C:/Users/wkeenan14/Documents/CHSTrafficCapstone/Etcstuff/coco.names"
 classNames = open(classesFile).read().strip().split('\n')
 print(classNames)
 print(len(classNames))
 
 ## Model Files
-modelConfiguration = 'yolov3-320.cfg'
-modelWeights = 'yolov3-320.weights'
+modelConfiguration = '"C:/Users/wkeenan14/Documents/CHSTrafficCapstone/yolov3-320.cfg'
+modelWeights = 'C:/Users/wkeenan14/Documents/CHSTrafficCapstone/yolov3-320.weights'
 # configure the network model
 net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 # Configure the network backend
@@ -81,4 +81,26 @@ def postProcess(outputs,img):
                     boxes.append([x,y,w,h])
                     classIds.append(classId)
                     confidence_scores.append(float(confidence))
+                    int((det[1]*height)-h/2)
+                    boxes.append([x,y,w,h])
+                    classIds.append(classId)
+                    confidence_scores.append(float(confidence))
+# Apply Non-Max Suppression
+    indices = cv2.dnn.NMSBoxes(boxes, confidence_scores, confThreshold,nmsThreshold)
+    # print(classIds)
+    for i in indices.flatten():
+        x, y, w, h = boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]
+        # print(x,y,w,h)
 
+        color = [int(c) for c in colors[classIds[i]]]
+        name = classNames[classIds[i]]
+        detected_classNames.append(name)
+
+
+        # Draw classname and confidence score 
+        cv2.putText(img,f'{name.upper()} {int(confidence_scores[i]*100)}%',
+                  (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+
+        # Draw bounding rectangle
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 1)
+        detection.append([x, y, w, h, required_class_index.index(classIds[i])])
