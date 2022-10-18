8c20831a75b6f87ba77aba90b517b937312192df
willhk10 <wkeenan14@charlottesvilleschools.org>
2022-10-18 11:40:51 -0400

    e

---
+++ Code/tensorflow10-18.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+import tensorflow as tf
+from tensorflow.keras import layers
