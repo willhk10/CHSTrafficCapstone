1c21e3af249745ac6bd98ed787165e9c9640903b
willhk10 <wkeenan14@charlottesvilleschools.org>
2022-10-18 12:12:18 -0400

    yes

--- Code/tensorflow10-18.py
+++ Code/tensorflow10-18.py
@@ -6,3 +6,56 @@ np.set_printoptions(precision=3, suppress=True)
 
 import tensorflow as tf
 from tensorflow.keras import layers
+
+
+abalone_train = pd.read_csv(
+    "https://storage.googleapis.com/download.tensorflow.org/data/abalone_train.csv",
+    names=["Length", "Diameter", "Height", "Whole weight", "Shucked weight",
+           "Viscera weight", "Shell weight", "Age"])
+
+abalone_features = abalone_train.copy()
+abalone_labels = abalone_features.pop('Age')
+
+# pack features into numpy array
+abalone_features = np.array(abalone_features)
+abalone_features
+# make a regressional model to predict the age
+abalone_model = tf.keras.Sequential([
+  layers.Dense(64),
+  layers.Dense(1)
+])
+
+abalone_model.compile(loss = tf.keras.losses.MeanSquaredError(), optimizer = tf.keras.optimizers.Adam())
+abalone_model.fit(abalone_features, abalone_labels, epochs=10)
+
+normalize = layers.Normalization()
+normalize.adapt(abalone_features)
+norm_abalone_model = tf.keras.Sequential([
+  normalize,
+  layers.Dense(64),
+  layers.Dense(1)
+])
+
+norm_abalone_model.compile(loss = tf.keras.losses.MeanSquaredError(),
+                           optimizer = tf.keras.optimizers.Adam())
+
+norm_abalone_model.fit(abalone_features, abalone_labels, epochs=10)
+
+titanic = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
+titanic.head()
+titanic_features = titanic.copy()
+titanic_labels = titanic_features.pop('survived')
+
+# Create a symbolic input
+input = tf.keras.Input(shape=(), dtype=tf.float32)
+
+# Perform a calculation using the input
+result = 2*input + 1
+
+# the result doesn't have a value
+result
+
+calc = tf.keras.Model(inputs=input, outputs=result)
+
+print(calc(1).numpy())
+print(calc(2).numpy())
